74099190a924ea5bc8c598e875348d7f0fa96d37
Emily <emilytenshaw@gmail.com>
2020-10-15 20:48:58 -0400

    Initial commit

---
+++ checkV14.py
@@ -0,0 +1,33 @@
+import DnAutosegClass
+import DnAutosegClassSet
+import config
+import requests
+import json
+import NeuronObjectData
+import re
+token = config.token
+auth = config.CatmaidApiTokenAuth(token)
+
+project_id = config.project_id
+
+#run after pulling neurons of interest
+#compares SKIDs in autoseg to V14
+def checkV14SKID(neuronSet):
+    skids = []
+    v14skids = []
+    for i in neuronSet:
+        if i not in skids:
+            skids.append(i.skeletonID)
+    for skid in skids:
+        response = requests.get(
+            'https://neuropil.janelia.org/tracing/fafb/v14/{}/skeletons/{}/root'.format(project_id, skid),
+            auth=auth  # ,
+        )
+        if response.status_code == 200:
+            v14skids.append(skid)
+
+    for i in neuronSet:
+        if i.skeletonID in v14skids:
+            i.existV14 = "True"
+
+    return
